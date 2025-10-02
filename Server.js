5535aeb18f3ba58e25c0629b0a9e7894cb87f549
Shaikh Afaroz <shaikhafaroz76@gmail.com>
2025-10-02 23:32:31 +0530

    Add fixed Server.js with profile image upload route

--- Server.js
+++ Server.js
@@ -396,7 +396,7 @@ function generateOTP(length = 6) {
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: parseInt(process.env.SMTP_PORT, 10),
-  secure: process.env.SMTP_PORT === '465',  // Use SSL for port 465
+  secure: process.env.SMTP_PORT === '587',  // Use SSL for port 465
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
